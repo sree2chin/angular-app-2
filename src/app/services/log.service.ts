4c0e58a831513a81b3b9c0a378f4f3ca063ef13f
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-21 15:17:25 +0530

    53 finish up the state

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -13,12 +13,17 @@ export class LogService {
   private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
   selectedLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
   constructor() { 
-    this.logs = [
-      {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
-      {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:33:13')},
-      {id: '3', text: 'Added logs component', date: new Date('12/27/2017 12:00:23')}
-    ]
+    // this.logs = [
+    //   {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
+    //   {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:33:13')},
+    //   {id: '3', text: 'Added logs component', date: new Date('12/27/2017 12:00:23')}
+    // ]
+
+    this.logs = [];
   }
 
   getLogs(): Observable<Log[]> {
@@ -48,6 +53,10 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+  }
+
+  clearState() {
+    this.stateSource.next(true);
   }
 
 }
