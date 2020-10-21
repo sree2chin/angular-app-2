41cbe00f4af1a9f300dfd81bad96daf43c0b698f
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-21 14:40:58 +0530

    51 - Observables | use BehaviourSubject to communicate between 2 sibling components

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 import { Log } from '../models/Log';
 
 @Injectable()
 export class LogService {
   logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+  selectedLog = this.logSource.asObservable();
 
   constructor() { 
     this.logs = [
@@ -14,8 +21,12 @@ export class LogService {
     ]
   }
 
-  getLogs() {
-    return this.logs;
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
   }
 
 }
